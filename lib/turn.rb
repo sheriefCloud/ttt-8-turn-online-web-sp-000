9a2a528607c4ebefde4275f1dd8e6b544ea2ac11
Sherief Farid <sherief@farid.cloud>
2019-02-13 01:00:31 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -41,7 +41,7 @@ end
 def turn(board)
   puts "Please enter 1-9:"
   user_input = gets.strip
-  input_to_index(user_input)
+  index = input_to_index(user_input)
   valid_move(board,index)
 
   display_board(board)
