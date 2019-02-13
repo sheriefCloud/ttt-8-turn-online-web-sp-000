a883ec824d50db75b1666fcfd0e4b4b2642499a1
Sherief Farid <sherief@farid.cloud>
2019-02-13 01:02:20 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -39,10 +39,12 @@ def position_taken? (board,index)
 end
 
 def turn(board)
-  puts "Please enter 1-9:"
-  user_input = gets.strip
-  index = input_to_index(user_input)
-  valid_move?(board,index)
+  valid = false
+  until (valid == true)
+    puts "Please enter 1-9:"
+    user_input = gets.strip
+    index = input_to_index(user_input)
+    valid = valid_move?(board,index)
 
   display_board(board)
 end
